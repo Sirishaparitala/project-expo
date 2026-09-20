f07ec7322c7b63f83c9791efb3ab33da46ff9abf
Sirishaparitala <sirishaparitala98@gmail.com>
2026-09-20 16:47:40 +0530

    Add RootLayout component with keyboard provider

---
+++ _layout.tsx
@@ -0,0 +1,3 @@
+import { KeyboardProvider } from "react-native-keyboard-controller";
+export default function RootLayout() {
+        <Stack screenOptions={{ headerShown: false, animation: "fade" }} />
